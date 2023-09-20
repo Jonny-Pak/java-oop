612f0675d8900915de6c34585fd76b8e06109417
Ngọc Thuận <145453864+Jonny-Pak@users.noreply.github.com>
2023-09-20 19:59:09 +0700

    init commit

---
+++ Quản lý Sách/ngay.java
@@ -0,0 +1,29 @@
+public class ngay {
+    private int day;
+    private int month;
+    private int year;
+
+    public ngay(int d, int m, int y) {
+        this.day = d;
+        this.month = m;
+        this.year = y;
+    }
+    public int getDay() {
+        return this.day;
+    }
+    public void setDay(int day) {
+        this.day = day;
+    }
+    public int getMonth() {
+        return this.month;
+    }
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    public int getYear() {
+        return this.year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
